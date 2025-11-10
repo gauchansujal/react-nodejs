3ddff9f34b0e0f032cabb65d7182eaad6642dfd3
gauchansujal <151667820+gauchansujal@users.noreply.github.com>
2025-11-10 17:04:50 +0545

    added

--- my-react-app/src/register.jsx
+++ my-react-app/src/register.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./App.css";
-import "./login.css";
+import "./register.css";
 
 
 function Register() {
