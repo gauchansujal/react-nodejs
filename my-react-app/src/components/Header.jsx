d0a9ef191c72ff603cd0d381636e20b90d8eac8e
gauchansujal <151667820+gauchansujal@users.noreply.github.com>
2025-11-12 11:26:27 +0545

    added header

--- my-react-app/src/components/Header.jsx
+++ my-react-app/src/components/Header.jsx
@@ -1,9 +1,14 @@
 import {useState} from 'react';
-import './header.css';
+import './Header.css';
 
 function Header(){
     return(
         <div className ="container">
+            <p1>hpome</p1>
+            <p2>Marketplace</p2>
+            <p3>Trends</p3>
+            <p4>About</p4>
+            <p5>Contact</p5>
             <button  className="lButton">login</button>
             <button className="RButton">Register</button>
         </div>
--- my-react-app/src/pages/home.jsx
+++ my-react-app/src/pages/home.jsx
@@ -3,11 +3,13 @@ import Header from "../components/Header"; // ✅ capital H
 
 function Home() {
   return (
+     
     <div>
-      <Header /> {/* 👈 your header component */}
+     <Header /> 
       <h1>Hello World</h1>
     </div>
   );
+
 }
 
 export default Home;
