42e3404bdfe64184092ef3b17c02b7ae83de1d02
gauchansujal <151667820+gauchansujal@users.noreply.github.com>
2025-11-21 22:45:20 +0545

    fotter page edited

--- my-react-app/src/components/Fotter.jsx
+++ my-react-app/src/components/Fotter.jsx
@@ -3,8 +3,40 @@ import "./Fotter.css";
 function Footer (){
     return (
         <div className ="box" >
-            <h1>EcoTrade</h1>
-            <p>smart recycling marktetplace connecting individuasl and bussinesses for sustainable material exchange.</p>
+            <section class="head">
+                <h1>EcoTrade</h1>
+                <p className="paragraph">
+                <span className="break-after">smart recycling marktetplace </span> 
+                <span className="break-after">connecting individuasl and bussinesses</span>
+                <span className = "break-after">for sustainable material exchange.</span></p>
+                     
+          
+            
+            <aside class = "head2">
+                <h1>Quick Links</h1>
+                <ol className="list">
+                 <li>Home</li>
+                 <li>Marketplace</li>
+                 <li>Price Trends</li>
+                 <li>Request Pcikup</li>
+                 <li>Become a Colection</li>
+                 </ol>
+            </aside>
+            <aside className="head3">
+                <h1>Materials</h1>
+                <ol className="list">
+                    <li>Plastic</li>
+                    <li>Metal</li>
+                    <li>Paper</li>
+                    <li>Glass</li>
+                    <li>Electronics</li>
+                </ol>
+            </aside>
+
+            </section>
+    
+            
+       
 
         </div>
 
